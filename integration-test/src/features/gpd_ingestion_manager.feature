4470a3e239d719a3e12ccd7d2f2c7d0d0f4f206c
giomella <giomella@emeal.nttdata.com>
2024-11-19 14:32:08 +0100

    [PPANTT-169] updated scenario to test create update and delete for payment position

--- integration-test/src/features/gpd_ingestion_manager.feature
+++ integration-test/src/features/gpd_ingestion_manager.feature
@@ -1,9 +1,13 @@
 Feature: All about ingestion events consumed by Azure functions gpd-ingestion-manager
 
-  Scenario: an operation on payment position table in GPD database is published into data lake event hub
-    Given a payment position is created with id '11111121' and fiscal code '77777777777' published in GPD database
-    When the payment position operation has been properly published on data lake event hub after 10000 ms
-    Then the data lake topic returns the payment position with id '11111121-c'
-    And the payment position has id 11111121
-    And the payment position has the fiscal code tokenized
-    
+  Scenario: CRUD operations on payment position table in GPD database are published into data lake event hub
+    Given a payment position created with id '11111121' and fiscal code '77777777777' in GPD database
+    And an update on the same payment position in GPD database
+    And a delete on the same payment position in GPD database
+    When the payment position operations have been properly published on data lake event hub after 10000 ms
+    Then the data lake topic returns the payment position 'create' operation with id '11111121-c'
+    And the data lake topic returns the payment position 'update' operation with id '11111121-u'
+    And the data lake topic returns the payment position 'delete' operation with id '11111121-d'
+    And the operations have id 11111121
+    And the operations have the fiscal code tokenized
+    And the payment position update operation has the company name changed from before and after
--- integration-test/src/step_definitions/gpd_ingestion_manager_step.js
+++ integration-test/src/step_definitions/gpd_ingestion_manager_step.js
@@ -9,7 +9,9 @@ setDefaultTimeout(360 * 1000);
 
 // initialize variables
 this.paymentPositionId = null;
-this.paymentPosition = null;
+this.paymentPositionCreateOp = null;
+this.paymentPositionUpdateOp = null;
+this.paymentPositionDeleteOp = null;
 this.paymentPositionFiscalCode = null
 
 AfterAll(async function () {
@@ -28,27 +30,56 @@ After(async function () {
   this.paymentPositionFiscalCode = null;
 });
 
-Given('a payment position is created with id {string} and fiscal code {string} published in GPD database', async function (id, fiscalCode) {
+/////////////////////////////
+// Payment Positions steps //
+/////////////////////////////
+
+Given('a payment position created with id {string} and fiscal code {string} in GPD database', async function (id, fiscalCode) {
   await insertPaymentPosition(id, fiscalCode);
   this.paymentPositionId = id;
   this.paymentPositionFiscalCode = fiscalCode;
 });
 
-When('the payment position operation has been properly published on data lake event hub after {int} ms', async function (time) {
+Given('an update on the same payment position in GPD database', async function () {
+  await updatePaymentPosition(this.paymentPositionId);
+});
+
+Given('a delete on the same payment position in GPD database', async function () {
+  await deletePaymentPosition(this.paymentPositionId);
+});
+
+When('the payment position operations have been properly published on data lake event hub after {int} ms', async function (time) {
   // boundary time spent by azure function to process event
   await sleep(time);
 });
 
-Then('the data lake topic returns the payment position with id {string}', async function (id) {
+Then('the data lake topic returns the payment position {string} operation with id {string}', async function (operation, id) {
   let pp = await readFromRedisWithKey(id);
-  this.paymentPosition = JSON.parse(pp);
+  if (operation === "create") {
+    this.paymentPositionCreateOp = JSON.parse(pp);
+    assert.strictEqual(this.paymentPositionCreateOp.op, "c");
+  } else if (operation === "update") {
+    this.paymentPositionUpdateOp = JSON.parse(pp);
+    assert.strictEqual(this.paymentPositionUpdateOp.op, "u");
+  } else if (operation === "delete") {
+    this.paymentPositionDeleteOp = JSON.parse(pp);
+    assert.strictEqual(this.paymentPositionDeleteOp.op, "d");
+  }
 });
 
-Then('the payment position has id {int}', function (id) {
-  assert.strictEqual(this.paymentPosition.op, "c");
-  assert.strictEqual(this.paymentPosition.after.id, id);
+Then('the operations have id {int}', function (id) {
+  assert.strictEqual(this.paymentPositionCreateOp.after.id, id);
+  assert.strictEqual(this.paymentPositionUpdateOp.before.id, id);
+  assert.strictEqual(this.paymentPositionUpdateOp.after.id, id);
+  assert.strictEqual(this.paymentPositionDeleteOp.after.id, id);
 });
 
-Then('the payment position has the fiscal code tokenized', function () {
-  assert.notStrictEqual(this.paymentPosition.after.fiscalCode, this.paymentPositionFiscalCode);
+Then('the operations have the fiscal code tokenized', function () {
+  assert.notStrictEqual(this.paymentPositionCreateOp.after.fiscalCode, this.paymentPositionFiscalCode);
+  assert.notStrictEqual(this.paymentPositionUpdateOp.before.fiscalCode, this.paymentPositionFiscalCode);
+  assert.notStrictEqual(this.paymentPositionUpdateOp.after.fiscalCode, this.paymentPositionFiscalCode);
+});
+
+Then('the payment position update operation has the company name changed from before and after', function () {
+  assert.notStrictEqual(this.paymentPositionUpdateOp.after.companyName, this.paymentPositionUpdateOp.before.companyName);
 });
