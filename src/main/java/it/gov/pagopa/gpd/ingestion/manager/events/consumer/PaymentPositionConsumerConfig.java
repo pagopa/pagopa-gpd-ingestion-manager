6e1ce7df33212e369f1ff560b1162015a4462a3e
svariant <samuele.varianti@nttdata.com>
2026-06-18 17:24:28 +0200

    feat: [PIDM-2052] Define dead letter error handlers

--- src/main/java/it/gov/pagopa/gpd/ingestion/manager/events/consumer/PaymentPositionConsumerConfig.java
+++ src/main/java/it/gov/pagopa/gpd/ingestion/manager/events/consumer/PaymentPositionConsumerConfig.java
@@ -1,9 +1,11 @@
 package it.gov.pagopa.gpd.ingestion.manager.events.consumer;
 
+import it.gov.pagopa.gpd.ingestion.manager.service.DeadLetterService;
 import it.gov.pagopa.gpd.ingestion.manager.service.IngestionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.support.ErrorMessage;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -17,4 +19,8 @@ public class PaymentPositionConsumerConfig {
         return ingestionService::ingestPaymentPositions;
     }
 
+    @Bean
+    public Consumer<ErrorMessage> deadLetterErrorHandler(DeadLetterService deadLetterService) {
+        return deadLetterService::sendToDeadLetter;
+    }
 }
